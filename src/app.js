1b42fe9140798f445531c794a4f917516e91963b
Twaran Gupta <twarangupta01@gmail.com>
2026-07-15 13:26:36 +0530

    creating config, server and app

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req,res) => {
+  res.status(200).json({
+    status: "ok"
+  })
+});
+
+module.exports = app;
